0e6417e446db1b4852756ad62243cb006522d47b
shuric-git <ulme@rambler.ru>
2022-04-10 22:23:08 +0300

    add swipers, style buttons, griding markup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+  // import Swiper JS
+  import Swiper, { Navigation, Pagination } from 'swiper';
 
+
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    modules: [Navigation, Pagination],
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });

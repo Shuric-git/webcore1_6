0b8ea7554a5b8e42667c37bbd17310954b6ee4ac
shuric-git <ulme@rambler.ru>
2022-04-14 23:57:09 +0300

    swipe my life, please

--- src/index.js
+++ src/index.js
@@ -1,19 +1,44 @@
   // import Swiper JS
-  import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+const slider = document.querySelector('.swiper');
+
+let mySlider;
+
+function mobileSlider() {
+  if (window.innerWidth <= 767 && slider.dataset.mobile == 'false') {
+    mySlider = new Swiper(slider, {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      loop: true,
+      modules: [Navigation, Pagination],
+
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
+
+    slider.dataset.mobile = 'true';
+  }
+
+  if (window.innerWidth > 767) {
+    slider.dataset.mobile = 'false';
+
+    if (slider.classList.contains('swiper-initialized')) {
+      mySlider.destroy();
+    }
+  }
+}
+
+mobileSlider();
+
+window.addEventListener('resize', () => {
+  mobileSlider();
+});
 
 
-  const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    loop: true,
-    modules: [Navigation, Pagination],
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+  // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },

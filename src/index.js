dc542a0eff8b5bb147be60431a601ab8ba4903be
shuric-git <ulme@rambler.ru>
2022-04-20 23:57:57 +0300

    make some js

--- src/index.js
+++ src/index.js
@@ -4,27 +4,56 @@ let modalCall = document.querySelector('.modal__call');
 let modalMessage = document.querySelector('.modal__message');
 let contactsCall = document.querySelectorAll('.contacts__call');
 let contactsMessage = document.querySelectorAll('.contacts__message');
+let burgerMenu = document.querySelector('.burger__menu');
+let burgerButton = document.querySelector('.burger-icon');
 let burgerList = document.querySelector('.burger__list');
 let burgerListItems = document.querySelectorAll('.burger__list-item');
+let modalOverlay = document.querySelector('.modal-overlay');
+let burgerOverlay = document.querySelector('.burger-overlay');
+
+burgerButton.addEventListener('click', ()=> {
+    console.log(burgerMenu)
+    burgerMenu.classList.toggle('burger-open');
+    burgerOverlay.classList.toggle('disabled');
+});
 
 closeCall.addEventListener('click', ()=> {
-    modalCall.classList.toggle('modal__closed')
+    modalCall.classList.toggle('modal__closed');
+    modalOverlay.classList.toggle('disabled');
 });
 
 closeMessage.addEventListener('click', ()=> {
-    modalMessage.classList.toggle('modal__closed')
+    modalMessage.classList.toggle('modal__closed');
+    modalOverlay.classList.toggle('disabled');
 });
 
 contactsCall.forEach( (item) => {
     item.addEventListener('click', () => {
-        modalCall.classList.toggle('modal__closed')
+        modalCall.classList.toggle('modal__closed');
+        modalOverlay.classList.toggle('disabled');
     });
 });
 
 contactsMessage.forEach( (item) => {
     item.addEventListener('click', () => {
         modalMessage.classList.toggle('modal__closed')
+        modalOverlay.classList.toggle('disabled');
     });
+});
+
+modalOverlay.addEventListener('click', (ev) => {
+    if (ev.target === modalOverlay) {
+        modalCall.classList.add('modal__closed');
+        modalMessage.classList.add('modal__closed');
+        modalOverlay.classList.add('disabled');
+    }
+});
+
+burgerOverlay.addEventListener('click', (ev) => {
+    if (ev.target === burgerOverlay) {
+        burgerMenu.classList.remove('burger-open');
+        burgerOverlay.classList.add('disabled');
+    }
 });
 
 burgerList.addEventListener('click', (ev) => {
